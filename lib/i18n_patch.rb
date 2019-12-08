47491c8cc5aede273ef51247995a11f12e18b3fd
Massimo Rossello <massimo.rossello@azcomtech.com>
2019-12-08 21:07:16 +0100

    Prepend-based patch, rails 5 compatible

--- lib/i18n_patch.rb
+++ lib/i18n_patch.rb
@@ -18,53 +18,24 @@
 require 'i18n'
 require 'redmine/i18n'
 
-# provide a fix for ::I18n.l calls module local Redmine::I18n.l instead of following ::I18n alias
-# this seems to affect Redmine 3.4, not 4.x
-# using explicit ::I18n.localize in format_date and format_time is not sufficient for issue to localize errors properly
-module Redmine
-  module I18n
-    def l(*args)
-      case args.size
-      when 1
-        if args.first.is_a?(Date) or args.first.is_a?(DateTime) or args.first.is_a?(Time)
-          ::I18n.localize(*args)
-        else
-          ::I18n.t(*args)
-        end
-      when 2
-        if args.first.is_a?(Date) or args.first.is_a?(DateTime) or args.first.is_a?(Time)
-          ::I18n.localize(*args)
-        elsif args.last.is_a?(Hash)
-          ::I18n.t(*args)
-        elsif args.last.is_a?(String)
-          ::I18n.t(args.first, :value => args.last)
-        else
-          ::I18n.t(args.first, :count => args.last)
-        end
-      else
-        raise "Translation string with multiple values: #{args.first}"
-      end
+# Redmine::I18n seems to have cases of localize called with other than Date, DateTime, Time in
+# methods like format_date. Sometimes the I18n::l alias is not called.
+module RedmineI18nPatch
+  # localize
+  def l(*args)
+    if args.first.is_a?(Date) or args.first.is_a?(DateTime) or args.first.is_a?(Time)
+      ::I18n.localize(*args)
+    else
+      super *args
     end
   end
 end
 
+
 # patch to add interpolated translations
 module I18nPatch
-  def self.included(base) # :nodoc:
-    base.extend(ClassMethods)
-
-    base.class_eval do
-      unloadable
-      class << self
-        alias_method_chain :translate, :interpolation
-        alias :t_with_interpolation :translate_with_interpolation
-        alias_method_chain :t, :interpolation
-      end
-    end
-  end
-
-  module ClassMethods
-    def translate_with_interpolation(*args)
+    # translate with terms interpolation
+    def translate(*args)
       options = args.last.is_a?(Hash) ? args.pop.dup : {}
       key = args.shift
       @overrides ||= Hash.new
@@ -80,51 +51,17 @@ module I18nPatch
         end
       end
       options.merge! @overrides[I18n.locale] if @overrides[I18n.locale]
-      translate_without_interpolation(key, options) rescue "translation missing: #{I18n.locale}.#{key}"
-    end
-
-    def format_date_with_fix(date)
-      return nil unless date
-      options = {}
-      options[:format] = Setting.date_format unless Setting.date_format.blank?
-      ::I18n.localize(date.to_date, options)
+      super(key, options) rescue "translation missing: #{I18n.locale}.#{key}"
     end
     
-    def format_time_with_fix(time, include_date=true, user=nil)
-      return nil unless time
-      user ||= User.current
-      options = {}
-      options[:format] = (Setting.time_format.blank? ? :time : Setting.time_format)
-      time = time.to_time if time.is_a?(String)
-      zone = user.time_zone
-      local = zone ? time.in_time_zone(zone) : (time.utc? ? time.localtime : time)
-      (include_date ? "#{format_date(local)} " : "") + ::I18n.localize(local, options)
-    end
-    
-    def l_with_fix(*args)
-      case args.size
-      when 1
-        ::I18n.t(*args)
-      when 2
-        if args.first.is_a?(Date)
-          ::I18n.localize(*args)
-        elsif args.last.is_a?(Hash)
-          ::I18n.t(*args)
-        elsif args.last.is_a?(String)
-          ::I18n.t(args.first, :value => args.last)
-        else
-          ::I18n.t(args.first, :count => args.last)
-        end
-      else
-        raise "Translation string with multiple values: #{args.first}"
-      end
-    end
-
-  end
+    alias :t :translate
 end
 
-unless I18n.included_modules.include?(I18nPatch)
-  I18n.send(:include, I18nPatch)
+unless I18n.singleton_class.included_modules.include?(I18nPatch)
+  I18n.singleton_class.send(:prepend, I18nPatch)
 end
 
+unless Redmine::I18n.included_modules.include?(RedmineI18nPatch)
+  Redmine::I18n.send(:prepend, RedmineI18nPatch)
+end
 
