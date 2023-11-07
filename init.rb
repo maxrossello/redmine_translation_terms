5a94cd5e71300e7db8d2689dfd503419502bd2ff
Massimo Rossello <counseling.mr@gmail.com>
2023-11-07 15:28:07 +0100

    Preparing version 5.1.0

--- init.rb
+++ init.rb
@@ -23,10 +23,10 @@ Redmine::Plugin.register :redmine_translation_terms do
   name 'Redmine Translation Terms plugin'
   author 'Massimo Rossello'
   description 'Applies configured translations to general terms (e.g. issue -> work item, project -> workspace)'
-  version '5.0.2'
+  version '5.1.0'
   url 'https://github.com/maxrossello/redmine_translation_terms.git'
   author_url 'https://github.com/maxrossello'
-  requires_redmine :version_or_higher => '5.0.2'
+  requires_redmine :version_or_higher => '5.1.0'
 end
 
 require_relative 'lib/i18n_patch'
